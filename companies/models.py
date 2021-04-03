da3f0f0f90db4d2ac6f689363013b2cb5cef02fc
Aleksandr Danilenko <rorik302@gmail.com>
2021-04-03 15:59:30 +0300

    форматирование

--- companies/models.py
+++ companies/models.py
@@ -5,23 +5,11 @@ from project.base.models import BaseModel
 
 
 class Company(BaseModel):
-    type = models.ForeignKey(
-        'companies.CompanyType', on_delete=models.PROTECT,
-        verbose_name='Правовая форма')
-    name_short = models.CharField(
-        max_length=255,
-        verbose_name='Сокращенное наименование',
-        unique=True)
-    name_full = models.CharField(
-        max_length=255,
-        verbose_name='Полное наименование',
-        unique=True)
-    is_customer = models.BooleanField(
-        default=False,
-        verbose_name='Может быть заказчиком')
-    is_transporter = models.BooleanField(
-        default=False,
-        verbose_name='Может быть перевозчиком')
+    type = models.ForeignKey('companies.CompanyType', on_delete=models.PROTECT, verbose_name='Правовая форма')
+    name_short = models.CharField(max_length=255, verbose_name='Сокращенное наименование', unique=True)
+    name_full = models.CharField(max_length=255, verbose_name='Полное наименование', unique=True)
+    is_customer = models.BooleanField(default=False, verbose_name='Может быть заказчиком')
+    is_transporter = models.BooleanField(default=False, verbose_name='Может быть перевозчиком')
     is_own = models.BooleanField(default=False, verbose_name='Своя компания')
 
     class Meta:
@@ -34,12 +22,8 @@ class Company(BaseModel):
 
 
 class CompanyType(BaseModel):
-    name_short = models.CharField(
-        max_length=255,
-        verbose_name='Сокращенное наименование')
-    name_full = models.CharField(
-        max_length=255,
-        verbose_name='Полное наименование')
+    name_short = models.CharField(max_length=255, verbose_name='Сокращенное наименование')
+    name_full = models.CharField(max_length=255, verbose_name='Полное наименование')
 
     class Meta:
         db_table = 'company_types'
@@ -51,21 +35,13 @@ class CompanyType(BaseModel):
 
 
 class Requisites(BaseModel):
-    company = models.ForeignKey(
-        Company, on_delete=models.CASCADE,
-        verbose_name='Организация')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='Организация')
     inn = models.CharField(max_length=20, blank=True, verbose_name='ИНН')
     kpp = models.CharField(max_length=20, blank=True, verbose_name='КПП')
     ogrn = models.CharField(max_length=20, blank=True, verbose_name='ОГРН')
-    legal_address = models.CharField(
-        max_length=255, blank=True,
-        verbose_name='Юридический адрес')
-    post_address = models.CharField(
-        max_length=255, blank=True,
-        verbose_name='Почтовый адрес')
-    fact_address = models.CharField(
-        max_length=255, blank=True,
-        verbose_name='Фактический адрес')
+    legal_address = models.CharField(max_length=255, blank=True, verbose_name='Юридический адрес')
+    post_address = models.CharField(max_length=255, blank=True, verbose_name='Почтовый адрес')
+    fact_address = models.CharField(max_length=255, blank=True, verbose_name='Фактический адрес')
     phone = PhoneNumberField(blank=True, verbose_name='Телефон')
     email = models.EmailField(blank=True, verbose_name='E-mail')
 
