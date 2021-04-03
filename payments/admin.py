1e84de9c28b6628a456fea820c0530c3ad27892d
Aleksandr Danilenko <rorik302@gmail.com>
2021-04-03 17:04:27 +0300

    PaymentForm, PaymentTerm

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import PaymentForm, PaymentTerm
+
+
+@admin.register(PaymentForm)
+class PaymentFormAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PaymentTerm)
+class PaymentTermAdmin(admin.ModelAdmin):
+    pass
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from project.base.models import BaseModel
+
+
+class PaymentForm(BaseModel):
+    text = models.TextField(verbose_name='Текст')
+
+    class Meta:
+        db_table = 'payment_forms'
+        verbose_name = 'Форма оплаты'
+        verbose_name_plural = 'Формы оплат'
+
+
+class PaymentTerm(BaseModel):
+    days_count = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name='Количество дней')
+    DAYS_TYPE_CHOICES = [
+        ('bank', 'банковские'),
+        ('calendar', 'календарные')
+    ]
+    days_type = models.CharField(max_length=50, blank=True, choices=DAYS_TYPE_CHOICES, verbose_name='Тип дней')
+    condition = models.CharField(max_length=255, verbose_name='Условия')
+
+    def __str__(self):
+        return f'{self.days_count} {self.days_type} {self.condition}'
+
+    class Meta:
+        db_table = 'payment_terms'
+        verbose_name = 'Срок оплаты'
+        verbose_name_plural = 'Сроки оплат'
