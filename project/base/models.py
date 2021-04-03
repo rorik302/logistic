a245c3f3205952b2cac1ff17a7d185824733ee9f
Aleksandr Danilenko <rorik302@gmail.com>
2021-04-03 16:23:50 +0300

    Phone

--- project/base/models.py
+++ project/base/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Phone(models.Model):
+    TYPE_CHOICE = [
+        ('mobile', 'Мобильный'),
+        ('desk', 'Стационарный'),
+    ]
+    type = models.CharField('Тип', max_length=50, choices=TYPE_CHOICE, default='mobile')
+    phone = PhoneNumberField('Номер телефона')
+    extension = models.CharField('Добавочный номер', max_length=20, blank=True)
 
     class Meta:
         abstract = True
