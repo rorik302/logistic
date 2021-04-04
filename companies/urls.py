3568eb6525e9cc6e78035689eadc7bdc4e92db0e
Aleksandr Danilenko <rorik302@gmail.com>
2021-04-04 13:57:07 +0300

    customers, transporters, own

--- companies/urls.py
+++ companies/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from companies.views import CompanyListCreateAPIView, CompanyRUDAPIView
+from companies.views import CompanyListCreateAPIView, CompanyRUDAPIView, CustomerListAPIView, TransporterListAPIView, \
+    OwnCompanyListAPIView
 
 app_name = 'companies'
 
 urlpatterns = [
+    path('customers/', CustomerListAPIView.as_view(), name='customers'),
+    path('own/', OwnCompanyListAPIView.as_view(), name='own_companies'),
+    path('transporters/', TransporterListAPIView.as_view(), name='transporters'),
     path('<slug:slug>/', CompanyRUDAPIView.as_view(), name='rud_company'),
     path('', CompanyListCreateAPIView.as_view(), name='list_create_companies')
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 
 from companies.models import Company
 from companies.serializers import CompanySerializer
@@ -13,3 +13,18 @@ class CompanyRUDAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     lookup_field = 'slug'
+
+
+class CustomerListAPIView(ListAPIView):
+    queryset = Company.objects.filter(is_customer=True)
+    serializer_class = CompanySerializer
+
+
+class TransporterListAPIView(ListAPIView):
+    queryset = Company.objects.filter(is_transporter=True)
+    serializer_class = CompanySerializer
+
+
+class OwnCompanyListAPIView(ListAPIView):
+    queryset = Company.objects.filter(is_own=True)
+    serializer_class = CompanySerializer
