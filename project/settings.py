0b32d12a51665f349607cefbac907361dc2a2e92
Aleksandr Danilenko <rorik302@gmail.com>
2021-04-30 18:49:57 +0300

    CORS

--- project/settings.py
+++ project/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'drf_spectacular',
     'rest_framework',
 
@@ -34,6 +35,7 @@ if DEBUG:
     INSTALLED_APPS.append('silk')
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,3 +101,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
